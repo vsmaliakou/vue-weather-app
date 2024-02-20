cf0e3f92faca397784187c873d3be03e6aaed780
vsmaliakou <vsmaliakou@yandex.by>
2024-02-20 19:25:01 +0300

    INIT: Init vue weather app project

---
+++ src/App.vue
@@ -0,0 +1,169 @@
+<script setup>
+import WeatherSummary from '@/components/WeatherSummary.vue';
+import Highlights from '@/components/Highlights.vue';
+import Coords from '@/components/Coords.vue';
+import Humidity from '@/components/Humidity.vue';
+import { API_KEY, BASE_URL } from '@/const/index';
+import { capitalizeFirstLetter } from '@/utils/index';
+import { ref, onBeforeMount, computed } from 'vue';
+
+const city = ref('Minsk');
+const weatherInfo = ref(null);
+const isError = computed(() => weatherInfo.value?.cod !== 200);
+
+async function getWeather() {
+  await fetch(`${BASE_URL}?q=${city.value}&units=metric&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      weatherInfo.value = data;
+    });
+}
+
+onBeforeMount(getWeather);
+</script>
+
+<template>
+<div class="page">
+  <main class="main">
+    <div class="container">
+      <div class="laptop">
+        <div class="sections">
+          <section
+            :class="['section', 'section-left', { 'section-error': isError }]"
+          >
+            <div class="info">
+              <div class="city-inner">
+                <input
+                  type="text"
+                  class="search"
+                  v-model="city"
+                  @keyup.enter="getWeather"
+                >
+              </div>
+              <WeatherSummary v-if="!isError" :weatherInfo="weatherInfo" />
+              <div v-else class="error">
+                <div class="error-title">
+                  Oooops! Something went wrong
+                </div>
+                <div v-if="weatherInfo?.message" class="error-message">
+                  {{ capitalizeFirstLetter(weatherInfo?.message) }}
+                </div>
+              </div>
+            </div>
+          </section>
+          <section v-if="!isError" class="section section-right">
+            <Highlights :weatherInfo="weatherInfo" />
+          </section> 
+        </div>
+        <div
+          class="sections"
+          v-if="!isError"
+        >
+          <Coords :coord="weatherInfo?.coord" />
+          <Humidity :humidity="weatherInfo?.main.humidity" />
+        </div>
+      </div>
+    </div>
+  </main>
+</div>
+</template>
+
+<style lang="sass" scoped>
+@import './assets/styles/main.sass'
+.page
+  position: relative
+  display: flex
+  justify-content: center
+  align-items: center
+  min-height: 100vh
+  padding: 20px 0
+  background-color: #59585d
+
+.laptop
+  width: 100%
+  padding: 20px
+  background-color: #0e100f
+  border-radius: 25px
+
+.sections
+  display: flex
+  width: 100%
+
+  @media (max-width: 767px)
+    flex-direction: column
+
+.section-left
+  width: 30%
+  padding-right: 10px
+
+  @media (max-width: 767px)
+    width: 100%
+    padding-right: 0
+
+  &.section-error
+    min-width: 235px
+    width: auto
+    padding-right: 0
+
+.section-right
+  width: 70%
+  padding-left: 10px
+
+  @media (max-width: 767px)
+    width: 100%
+    margin-top: 16px
+    padding-left: 0
+
+.city-inner
+  position: relative
+  display: inline-block
+  width: 100%
+
+  &::after
+    content: ''
+    position: absolute
+    top: 0
+    right: 10px
+    width: 25px
+    height: 25px
+    background: url('./assets/img/search.svg') no-repeat 50% 50%
+    background-size: contain
+    transform: translateY(50%)
+    cursor: pointer
+
+.info
+  height: 100%
+  padding: 16px
+  background: url('./assets/img/gradient-1.jpg') no-repeat 50% 50%
+  background-size: cover
+  border-radius: 25px
+
+.search
+  width: 100%
+  padding: 16px
+  font-family: 'Inter', Arial, sans-serif
+  color: $white
+  background-color: rgba(0, 0, 0, 0.75)
+  border-radius: 16px
+  border: none
+  outline: none
+  cursor: pointer
+
+.section-bottom
+  width: 50%
+  margin-top: 16px
+
+  @media (max-width: 767px)
+    width: 100%
+
+.error
+  padding-top: 20px
+
+  &-title
+    font-size: 18px
+    font-weight: 700
+
+  &-message
+    padding-top: 10px
+    font-size: 14px
+</style>
---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+import { PRESSURE_UNITS } from "@/const";
+
+export const capitalizeFirstLetter = (str) => {
+  if (!str) {
+    return '';
+  }
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const getPressureMm = (hpa) => {
+  return Math.round(hpa * PRESSURE_UNITS);
+};
+
+export const getTime = (seconds) => {
+  return new Date(seconds * 1000).toLocaleTimeString('ru-RU', {
+    timeZone: 'Atlantic/Reykjavik',
+  });
+};
